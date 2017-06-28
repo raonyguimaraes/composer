f4a8179be650fdc824f2f19a6521b2b238a647b1
Maja Nedeljković <maya.nedeljko@gmail.com>
2017-06-29 00:44:30 +0200

    Circular loader style fixes

--- src/app/ui/circular-loader/circular-loader.component.ts
+++ src/app/ui/circular-loader/circular-loader.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ChangeDetectionStrategy} from "@angular/core";
 
 @Component({
     selector: "ct-circular-loader",
-    template: `<div class="loader"></div>`,
+    template: `<div class="loader-container"><div class="loader"></div></div>`,
     styleUrls: ["./circular-loader.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
